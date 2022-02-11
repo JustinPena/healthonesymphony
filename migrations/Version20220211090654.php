fef532880f976d242abf8d25cb395769ad9cd9ec
JustinPena <302862351@student.rocmondriaan.nl>
2022-02-11 10:39:53 +0100

    Week 2 finish

---
+++ migrations/Version20220211090654.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220211090654 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE category ADD name VARCHAR(255) NOT NULL, ADD picture VARCHAR(255) NOT NULL, ADD description VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE product ADD category_id INT NOT NULL, ADD name VARCHAR(255) NOT NULL, ADD picture VARCHAR(255) NOT NULL, ADD description VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_D34A04AD12469DE2 ON product (category_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE category DROP name, DROP picture, DROP description');
+        $this->addSql('ALTER TABLE product DROP FOREIGN KEY FK_D34A04AD12469DE2');
+        $this->addSql('DROP INDEX IDX_D34A04AD12469DE2 ON product');
+        $this->addSql('ALTER TABLE product DROP category_id, DROP name, DROP picture, DROP description');
+    }
+}
